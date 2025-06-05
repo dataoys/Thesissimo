c9348635cf514aee52f521807350ddd1a5be44e3
318215 <318215@studenti.unimore.it>
2025-06-05 09:03:08 +0000

    PROVA DEFINITIVA

--- forces-7427867e0c0aa40128b3f01dd26a1945c3c08359-doc-doxygen-awesome-css/doc/doxygen-awesome-css/html/search/searchdata.js
+++ forces-7427867e0c0aa40128b3f01dd26a1945c3c08359-doc-doxygen-awesome-css/doc/doxygen-awesome-css/html/search/searchdata.js
@@ -1,30 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "_abcdefgijlmnopqrstuw🌐",
-  1: "bcdpqsuw",
-  2: "_bcdmprsuw",
-  3: "abcdegijlmprsu",
-  4: "abcdefijmnoprstuw",
-  5: "e🌐"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "namespaces",
-  2: "files",
-  3: "functions",
-  4: "variables",
-  5: "pages"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Namespaces",
-  2: "Files",
-  3: "Functions",
-  4: "Variables",
-  5: "Pages"
 };
 
